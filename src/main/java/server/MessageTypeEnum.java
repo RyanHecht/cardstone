78e5d58a294ed25abfb63212e310e0a13420b45b
Ryan Hecht <ryanhecht.tse@gmail.com>
2017-04-19 14:39:36 -0400

    added new messagetypeenums

--- src/main/java/server/MessageTypeEnum.java
+++ src/main/java/server/MessageTypeEnum.java
@@ -4,12 +4,43 @@ public enum MessageTypeEnum {
 
   // Updates of board state (Server -> Client)
   BOARD_STATE,
-  // Animations to play (Server -> Client)
+
+  // whether the client understood the sent board state (Client -> Server)
+  UNDERSTOOD_BOARD_STATE,
+
+  // predefined animation to play (Server -> Client)
   ANIMATION,
-  // A user's move (Client -> Server)
-  USER_INPUT,
+
+  // Explicitly defines animation to play (Server -> Client)
+  EXPLICIT_ANIMATION,
+
+  // A user targeted a card with another card (Client -> Server)
+  TARGETED_CARD,
+
+  // A user targeted a player with a card (Client -> Server)
+  TARGETED_PLAYER,
+
+  // A user attempted to play a card (Client -> Server)
+  ATTEMPTED_TO_PLAY,
+
+  // the server asks a player to choose from a list of cards (Server -> Client)
+  CHOOSE_REQUEST,
+
+  // client responds to choose request (Client -> Server)
+  CHOOSE_RESPONSE,
+
+  // the server asks a player to target something (Server -> Client)
+  TARGET_REQUEST,
+
+  // client responds to target request (Client -> Server)
+  TARGET_RESPONSE,
+
+  // whether a client's action was okay or not (Server -> Client)
+  ACTION_OK,
+
   // Request for the user's Id (Server -> Client)
   ID_REQUEST,
+
   // Response containing the user's Id (Client -> Server)
   ID_RESPONSE
 
