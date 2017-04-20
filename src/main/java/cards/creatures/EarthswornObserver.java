84727d2be98da31950d263b0c0953802d46fb1c5
KaushikRaghuNimmagadda <kaushik_nimmagadda@brown.edu>
2017-04-20 15:12:34 -0400

    Fixed CantAttackCreature so it now takes correct params and implements card

--- src/main/java/cards/creatures/EarthswornObserver.java
+++ src/main/java/cards/creatures/EarthswornObserver.java
@@ -1,7 +1,6 @@
 package cards.creatures;
 
 import cardgamelibrary.Board;
-import cardgamelibrary.Card;
 import cardgamelibrary.CardType;
 import cardgamelibrary.Creature;
 import cardgamelibrary.Effect;
@@ -9,36 +8,33 @@ import cardgamelibrary.ElementType;
 import cardgamelibrary.ManaPool;
 import cardgamelibrary.Zone;
 import cards.templates.CantAttackCreature;
-import effects.EmptyEffect;
 import game.Player;
 
-public class EarthswornObserver extends Creature implements CantAttackCreature{
+public class EarthswornObserver extends Creature implements CantAttackCreature {
 
-	
-	//Is it gonna attack or not?
-	private static final ManaPool defaultCost = new ManaPool(25,0,0,1,0,0);
-	private static final String defaultImage = "images/EarthswornObserver.png";
-	private static final String defaultName = "Earthsworn Observer";
-	private static final String defaultText = "Can't attack. Whenever a minion with Earth in its cost dies, gain 2 health.";
-	private static final int defaultHealth = 8;
-	private static final int defaultAttack = 0;
-	private static final CardType defaultType = CardType.CREATURE;
-	public EarthswornObserver(Player owner){
-		super(defaultAttack,defaultHealth,defaultCost, defaultImage, owner, defaultName,
-				defaultText, defaultType);
+	// Is it gonna attack or not?
+	private static final ManaPool	defaultCost		= new ManaPool(25, 0, 0, 1, 0, 0);
+	private static final String		defaultImage	= "images/EarthswornObserver.png";
+	private static final String		defaultName		= "Earthsworn Observer";
+	private static final String		defaultText		= "Can't attack. Whenever a minion with Earth in its cost dies, gain 2 health.";
+	private static final int			defaultHealth	= 8;
+	private static final int			defaultAttack	= 0;
+	private static final CardType	defaultType		= CardType.CREATURE;
+
+	public EarthswornObserver(Player owner) {
+		super(defaultAttack, defaultHealth, defaultCost, defaultImage, owner, defaultName, defaultText, defaultType);
 	}
-	
+
 	@Override
-	public Effect onCreatureDeath(Creature cr, Zone z){
-		if(z == Zone.CREATURE_BOARD){
-			if(cr.getCost().getElement(ElementType.EARTH) > 0){
+	public Effect onCreatureDeath(Creature cr, Zone z) {
+		if (z == Zone.CREATURE_BOARD) {
+			if (cr.getCost().getElement(ElementType.EARTH) > 0) {
 				return (Board board) -> {
-					board.changeCreatureHealth(this,2);
+					board.changeCreatureHealth(this, 2);
 				};
 			}
 		}
 		return super.onCreatureDeath(cr, z);
 	}
-	
-	
+
 }
--- src/main/java/cards/templates/CantAttackCreature.java
+++ src/main/java/cards/templates/CantAttackCreature.java
@@ -1,12 +1,16 @@
 package cards.templates;
 
+import cardgamelibrary.Card;
 import cardgamelibrary.Effect;
+import cardgamelibrary.Zone;
 import effects.EmptyEffect;
+import game.Player;
 
-public interface CantAttackCreature {
+public interface CantAttackCreature extends Card {
 
-	default public Effect onTurnStart(){
+	@Override
+	default public Effect onTurnStart(Player p, Zone z) {
 		return EmptyEffect.create();
 	}
-	
+
 }
