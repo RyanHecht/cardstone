d14ddb5071491a160706fbc70e45f3ca3769c20b
Josh Pattiz <Josh Pattiz>
2017-04-26 01:36:43 -0400

    fixed incorrect display of resources on board

--- src/main/resources/static/js/server.js
+++ src/main/resources/static/js/server.js
@@ -129,11 +129,6 @@ class Server{
 	}
 
 
-
-
-
-
-
 	messageReceived(message){
 		switch(message.type){
 			case MESSAGE_TYPE.BOARD_STATE:
@@ -191,10 +186,10 @@ class Server{
 	}
 
 	boardReceived(data){
-		console.log(data)
 		this.setPlayers(data.player1,data.player2);
 		wholeBoard.changeFeature("p1Deck",data.board.deckOne);
 		wholeBoard.changeFeature("p2Deck",data.board.deckTwo);
+        wholeBoard.buildResZones();
 		cardCache.repairFrom(data.board);
 		wholeBoard.getFromCache(data.board);
 		redrawAll();
