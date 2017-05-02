c78ced3ddcd7da32162e8a58bc6bdcb8de819518
Ryan Hecht <ryanhecht.tse@gmail.com>
2017-05-01 22:34:18 -0400

    removed another test board

--- src/main/java/server/CommsWebSocket.java
+++ src/main/java/server/CommsWebSocket.java
@@ -1,19 +1,11 @@
 package server;
 
 import cardgamelibrary.Board;
-import cardgamelibrary.OrderedCardCollection;
-import cardgamelibrary.Zone;
-import cards.SkyWhaleCreature;
-import dummycards.StubCreature;
-
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import game.Game;
 import game.GameManager;
-import game.Player;
-import game.PlayerType;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -323,46 +315,6 @@ public class CommsWebSocket {
         session.getRemote().sendString(GSON.toJson(obj));
       }
     }
-  }
-
-  private static Game testBoard() {
-    Player pOne = new Player(30, PlayerType.PLAYER_ONE, 0);
-    Player pTwo = new Player(30, PlayerType.PLAYER_TWO, 1);
-    OrderedCardCollection deckOne = new OrderedCardCollection(Zone.DECK, pOne);
-    OrderedCardCollection deckTwo = new OrderedCardCollection(Zone.DECK, pTwo);
-    Board b1 = new Board(deckOne, deckTwo);
-    StubCreature c1 = new StubCreature(pOne);
-    StubCreature c2 = new StubCreature(pTwo);
-    SkyWhaleCreature c3 = new SkyWhaleCreature(pOne);
-    OrderedCardCollection playerOneCreatures = new OrderedCardCollection(
-        Zone.CREATURE_BOARD, pOne);
-    OrderedCardCollection playerTwoCreatures = new OrderedCardCollection(
-        Zone.CREATURE_BOARD, pTwo);
-
-    playerOneCreatures.add(c1);
-    playerOneCreatures.add(c3);
-    playerTwoCreatures.add(c2);
-
-    b1.setOcc(playerOneCreatures);
-    b1.setOcc(playerTwoCreatures);
-
-    SkyWhaleCreature c4 = new SkyWhaleCreature(pTwo);
-    OrderedCardCollection playerOneCreatures2 = new OrderedCardCollection(
-        Zone.CREATURE_BOARD, pOne);
-    OrderedCardCollection playerTwoCreatures2 = new OrderedCardCollection(
-        Zone.CREATURE_BOARD, pTwo);
-
-    playerOneCreatures2.add(c1);
-    playerOneCreatures2.add(c3);
-    playerTwoCreatures2.add(c2);
-    playerTwoCreatures2.add(c4);
-    b1.setOcc(playerOneCreatures2);
-    b1.setOcc(playerTwoCreatures2);
-
-    Game game = new Game(new ArrayList<String>(), new ArrayList<String>(), 0,
-        1);
-    game.setBoard(b1);
-    return game;
   }
 
 }
