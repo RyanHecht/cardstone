87620146e83ff7b05546ff61f4d298e625a4e117
Josh Pattiz <Josh Pattiz>
2017-05-03 00:53:44 -0400

    moved the cardDraw around

--- src/main/resources/static/js/animationsMaker.js
+++ src/main/resources/static/js/animationsMaker.js
@@ -4,6 +4,37 @@ function killAnimBox(){
     box.hide();
 }
 
+class cardDrawnAnimation extends animation{
+    
+    constructor(){
+		super();
+        let off = $(".centerClockRect").offset(); 
+        this.setStartLoc(off.left,off.top);
+        this.speed = .6;
+	}
+    
+    create(){
+        let drawables = [];
+        let drawable = new movingDrawable("card",this.startX,this.startY,this.goalX,this.goalY,"red",this.speed,5);
+        drawables.push(drawable);
+        return drawables;
+    }
+    setStartLoc(startX, startY){
+        this.startX = startX;
+        this.startY = startY;
+    }
+    setTarget(isMe){
+        let wind = $(window);
+        this.goalX = wind.width() / 2;
+        if(isMe){
+            this.goalY = wind.height() - CARD_DRAW_ANIM_HEIGHT_OFFSET;
+        }
+        else{
+            this.goalY = 0;
+        }
+    }
+}
+
 function getDrawnCardAnimation(isMe){
     let anim = new cardDrawnAnimation();
     anim.setTarget(isMe);
@@ -103,33 +134,3 @@ class animationsMaker{
 const CARD_DRAW_ANIM_HEIGHT_OFFSET = 80;
 const CARD_DRAW_ANIM_HEIGHT = 140;
 
-class cardDrawnAnimation extends animation{
-    
-    constructor(){
-		super();
-        let off = $(".centerClockRect").offset(); 
-        this.setStartLoc(off.left,off.top);
-        this.speed = .6;
-	}
-    
-    create(){
-        let drawables = [];
-        let drawable = new movingDrawable("card",this.startX,this.startY,this.goalX,this.goalY,"red",this.speed,5);
-        drawables.push(drawable);
-        return drawables;
-    }
-    setStartLoc(startX, startY){
-        this.startX = startX;
-        this.startY = startY;
-    }
-    setTarget(isMe){
-        let wind = $(window);
-        this.goalX = wind.width() / 2;
-        if(isMe){
-            this.goalY = wind.height() - CARD_DRAW_ANIM_HEIGHT_OFFSET;
-        }
-        else{
-            this.goalY = 0;
-        }
-    }
-}
