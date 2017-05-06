d2074138d0b15b350ff218fd8a67bc6d3a69822d
KaushikRaghuNimmagadda <kaushik_nimmagadda@brown.edu>
2017-05-06 02:20:31 -0400

    Sand tomb done :D

--- src/main/java/cards/SandTomb.java
+++ src/main/java/cards/SandTomb.java
@@ -1,6 +1,5 @@
 package cards;
 
-import cardgamelibrary.Board;
 import cardgamelibrary.Card;
 import cardgamelibrary.CardType;
 import cardgamelibrary.Creature;
@@ -8,6 +7,8 @@ import cardgamelibrary.Effect;
 import cardgamelibrary.ManaPool;
 import cardgamelibrary.SpellCard;
 import cardgamelibrary.Zone;
+import effects.SummonEffect;
+import effects.TransformEffect;
 import game.Player;
 import templates.OnOwnDeathCard;
 import templates.TargetsOtherCard;
@@ -18,11 +19,11 @@ public class SandTomb extends SpellCard implements TargetsOtherCard {
 		super(defaultCost, defaultImage, owner, defaultName, defaultText, defaultType);
 	}
 
-	private static final ManaPool	defaultCost		= new ManaPool(25, 0, 0, 1, 0, 0);
-	private static final String		defaultImage	= "images/SandTomb.jpg";
-	private static final String		defaultName		= "Sand Tomb";
-	private static final String		defaultText		= "Imprison an enemy minion in a 0/8 Tomb with taunt on your side of the board. When the Tomb is destroyed, return the minion.";
-	private static final CardType	defaultType		= CardType.SPELL;
+	private static final ManaPool defaultCost = new ManaPool(25, 0, 0, 1, 0, 0);
+	private static final String defaultImage = "images/SandTomb.jpg";
+	private static final String defaultName = "Sand Tomb";
+	private static final String defaultText = "Imprison an enemy minion in a 0/8 Tomb with taunt on your side of the board. When the Tomb is destroyed, return the minion.";
+	private static final CardType defaultType = CardType.SPELL;
 
 	@Override
 	public boolean cardValidTarget(Card card, Zone targetIn) {
@@ -42,22 +43,18 @@ public class SandTomb extends SpellCard implements TargetsOtherCard {
 	public Effect impactCardTarget(Card target, Zone targetIn) {
 		assert cardValidTarget(target, targetIn);
 		Creature tomb = new Tomb(target, getOwner());
-		return (Board board) -> {
-			// transform the card.
-			board.transformCard(target, tomb, Zone.BOARD);
-			board.addCardToOcc(this, board.getOcc(getOwner(), Zone.GRAVE), board.getOcc(getOwner(), Zone.HAND));
-		};
+		return new TransformEffect(target, tomb, Zone.CREATURE_BOARD);
 	}
 
 	private static class Tomb extends Creature implements OnOwnDeathCard {
-		private static final ManaPool	defaultCost		= new ManaPool(0, 0, 0, 1, 0, 0);
-		private static final String		defaultImage	= "images/Tomb.jpg";
-		private static final String		defaultName		= "Tomb";
-		private static final String		defaultText		= "Destroy this to free the imprisoned minion!";
-		private static final int			defaultHealth	= 8;
-		private static final int			defaultAttack	= 0;
-		private static final CardType	defaultType		= CardType.CREATURE;
-		Card													resummon;
+		private static final ManaPool defaultCost = new ManaPool(0, 0, 0, 1, 0, 0);
+		private static final String defaultImage = "images/Tomb.jpg";
+		private static final String defaultName = "Tomb";
+		private static final String defaultText = "Destroy this to free the imprisoned minion!";
+		private static final int defaultHealth = 8;
+		private static final int defaultAttack = 0;
+		private static final CardType defaultType = CardType.CREATURE;
+		Card resummon;
 
 		private Tomb(Card resummon, Player owner) {
 			super(defaultAttack, defaultHealth, defaultCost, defaultImage, owner, defaultName, defaultText, defaultType);
@@ -66,9 +63,7 @@ public class SandTomb extends SpellCard implements TargetsOtherCard {
 
 		@Override
 		public Effect onDeathEffect(Zone z) {
-			return (Board board) -> {
-				board.summonCard(resummon, Zone.CREATURE_BOARD);
-			};
+			return new SummonEffect(resummon, Zone.CREATURE_BOARD);
 		}
 
 	}
