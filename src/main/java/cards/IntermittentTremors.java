3c1c268fdd872a5df35a37e6a7e87e44d5a7aa45
KaushikRaghuNimmagadda <kaushik_nimmagadda@brown.edu>
2017-05-07 05:11:59 -0400

    Undermine and IntermittentTremors now correctly utilize the new and amazing AoeDamageEffect. Praise be to FunctionThree and the glorious lambda package

--- src/main/java/cards/IntermittentTremors.java
+++ src/main/java/cards/IntermittentTremors.java
@@ -35,8 +35,8 @@ public class IntermittentTremors extends AuraCard {
 			if (turnsToTremor == 0) {
 				turnsToTremor = 3;
 
-				return new AoeDamageEffect(0, 0, 0, false, false, this, FunctionThreeMaker.targetsAllCreatures(),
-						FunctionThreeMaker.determineCreatureDamage(1));
+				return new AoeDamageEffect(1, 0, 0, false, false, this, FunctionThreeMaker.targetsAllCreatures(),
+						FunctionThreeMaker.determineCreatureDamage(0));
 			} else {
 				turnsToTremor--;
 			}
