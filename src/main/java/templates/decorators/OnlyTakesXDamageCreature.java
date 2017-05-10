0cf6e55a392f263750134994d93db46b9b5a5b77
Josh Pattiz <Josh Pattiz>
2017-05-09 20:09:06 -0400

    fixed petrify i think

--- src/main/java/templates/decorators/OnlyTakesXDamageCreature.java
+++ src/main/java/templates/decorators/OnlyTakesXDamageCreature.java
@@ -27,6 +27,11 @@ public class OnlyTakesXDamageCreature extends CreatureWrapper {
 	}
 	
 	public Effect getNewProposition(Effect e, Zone z){
+		if(e.getType() == EffectType.CARD_DAMAGED){
+			CardDamageEffect cde = (CardDamageEffect) e;
+			cde.setDamage(this.allowedDamage);
+			return cde;
+		}
 		return e;
 	}
 
